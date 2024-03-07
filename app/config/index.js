79e214e7abf4d155c0fe8b0af3bef5620e5ae8b8
Hoa Pham <quavndr123@gmail.com>
2024-03-08 02:29:40 +0700

    updates

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,12 @@
 const config = {
   app: {
     port: process.env.PORT || 3000,
-  }
+  },
+  db: {
+    uri:
+      process.env.MONGODB_URI ||
+      "mongodb+srv://contactbook:zetmins012@cluster0.dkkowcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  },
 };
 
 module.exports = config;
